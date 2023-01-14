660bffc4c514a3dabf8db40422a655200f65ba3a
Ido Shimshi <idshimshi@gmail.com>
2023-01-14 19:30:28 +0200

    initial upload plys kmeans and single linkage

---
+++ Assignment3/kmeans.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+
+def kmeans(X, k, t):
+    """
+    :param X: numpy array of size (m, d) containing the test samples
+    :param k: the number of clusters
+    :param t: the number of iterations to run
+    :return: a column vector of length m, where C(i) ∈ {1, . . . , k} is the identity of the cluster in which x_i has been assigned.
+    """
+    centers = np.random.rand(k,X.shape[1])
+    clusters_old = split_to_cluster(X,centers)
+    clusters_new = []
+    for i in range(t):
+        if np.array_equal(clusters_old,clusters_new):
+            print("converged")
+            return np.array(clusters_old).reshape(-1,1) 
+        clusters_old = clusters_new
+        clusters_new = split_to_cluster(X,centers)
+        centers = calc_centroids(clusters_new,X,k)  
+    return np.array(clusters_new).reshape(-1,1) 
+
+
+def split_to_cluster(X,centers):
+    clusters = []
+    for i in range(X.shape[0]):
+        distances = []
+        for j in range(centers.shape[0]):
+            distances.append(np.linalg.norm(X[i]-centers[j]))
+        clusters.append(np.argmin(distances))
+    return np.array(clusters)
+
+def calc_centroids(clusters,X,k):
+    centroids = []
+    for i in range(k):
+        centroids.append(np.mean(X[clusters == i],axis=0))
+    return np.array(centroids)
+
+def simple_test():
+    # load sample data (this is just an example code, don't forget the other part)
+    data = np.load('mnist_all.npz')
+    X = np.concatenate((data['train0'], data['train1']))
+    m, d = X.shape
+    # run K-means
+    c = kmeans(X, k=10, t=10)
+
+    print(c)
+    print(c.shape)
+
+    assert isinstance(c, np.ndarray), "The output of the function softsvm should be a numpy array"
+    assert c.shape[0] == m and c.shape[1] == 1, f"The shape of the output should be ({m}, 1)"
+
+if __name__ == '__main__':
+    # before submitting, make sure that the function simple_test runs without errors
+    simple_test()
+
+    # here you may add any code that uses the above functions to solve question 2
---
+++ Assignment3/singlelinkage.py
@@ -0,0 +1,56 @@
+import numpy as np
+import scipy as sio
+from scipy.spatial import distance_matrix
+
+
+def singlelinkage(X, k):
+    """
+    :param X: numpy array of size (m, d) containing the test samples
+    :param k: the number of clusters
+    :return: a column vector of length m, where C(i) ∈ {1, . . . , k} is the identity of the cluster in which x_i has been assigned.
+    """
+    m = X.shape[0]
+    cluster_distances = distance_matrix(X,X,2)
+    np.fill_diagonal(cluster_distances, np.inf)
+    clusters = range(m)
+    for i in range(m-k):
+        argmin = np.unravel_index(cluster_distances.argmin(), cluster_distances.shape)
+        for i in range(len(clusters)):
+            if clusters[i] == argmin[0]:
+                clusters[i] == argmin[1]
+
+        new_distances = [min(cluster_distances[i,argmin[0]],cluster_distances[i,argmin[1]]) if cluster_distances[i,argmin[1]] != np.inf else np.inf for i in range(m)]
+        cluster_distances[:,argmin[1]] = new_distances
+        cluster_distances[argmin[1]] = new_distances
+        cluster_distances[:,argmin[0]] = np.full((m),np.inf)
+        cluster_distances[argmin[0]] = np.full((m),np.inf)
+        
+
+    return np.array(clusters).reshape(-1,1)
+
+
+def simple_test():
+    # load sample data (this is just an example code, don't forget the other part)
+    data = np.load('mnist_all.npz')
+    # X = np.concatenate((data['train0'], data['train1']))
+    data_i = data[f'train0']
+    np.random.shuffle(data_i)
+    X = data_i[:3]
+    for i in range(1,10):
+        data_i = data[f'train{i}']
+        np.random.shuffle(data_i)
+        X = np.concatenate((X,data_i[:30]))
+    m, d = X.shape
+
+    # run single-linkage
+    c = singlelinkage(X, k=10)
+
+    assert isinstance(c, np.ndarray), "The output of the function softsvm should be a numpy array"
+    assert c.shape[0] == m and c.shape[1] == 1, f"The shape of the output should be ({m}, 1)"
+
+
+if __name__ == '__main__':
+    # before submitting, make sure that the function simple_test runs without errors
+    simple_test()
+
+    # here you may add any code that uses the above functions to solve question 2
